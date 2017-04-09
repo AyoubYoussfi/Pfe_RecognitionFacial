b5c917e36732781939de73204b284cad61ae8d8f
Android <y.ayoub95@live.com>
2017-04-10 00:21:51 +0100

    Second modification

--- facerecognition/src/main/java/org/opencv/javacv/facerecognition/Activity/RateThisApp.java
+++ facerecognition/src/main/java/org/opencv/javacv/facerecognition/Activity/RateThisApp.java
@@ -11,6 +11,6 @@ public class RateThisApp extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rate_this_app);
+        //setContentView(R.layout.activity_rate_this_app);
     }
 }
--- facerecognition/src/main/java/org/opencv/javacv/facerecognition/MainActivity.java
+++ facerecognition/src/main/java/org/opencv/javacv/facerecognition/MainActivity.java
@@ -2,6 +2,7 @@ package org.opencv.javacv.facerecognition;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,6 +10,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import org.opencv.javacv.facerecognition.Activity.AboutUs;
+import org.opencv.javacv.facerecognition.Activity.SetIntruderSelfie;
+import org.opencv.javacv.facerecognition.Activity.Training;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -24,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void training(View view) {
+        startActivity(new Intent(MainActivity.this, Training.class));
     }
 
     public void setPin(View view) {
@@ -97,11 +103,43 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void setIntruderSelfie(View view) {
+        startActivity(new Intent(MainActivity.this, SetIntruderSelfie.class));
     }
 
     public void RateApp(View view) {
+        LayoutInflater li = LayoutInflater.from(context);
+        View promptsView = li.inflate(R.layout.activity_rate_this_app, null);
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+        // set prompts.xml to alertdialog builder
+        alertDialogBuilder.setView(promptsView);
+
+
+        // set dialog message
+        alertDialogBuilder
+                .setCancelable(false)
+                .setPositiveButton("OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                // get user input and set it to result
+                                // edit text
+
+                                //result.setText(userInput.getText());
+                            }
+                        })
+                .setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                dialog.cancel();
+                            }
+                        });
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // show it
+        alertDialog.show();
     }
 
     public void AboutUs(View view) {
+        startActivity(new Intent(MainActivity.this, AboutUs.class));
     }
 }
